a066c0d7b45dc66e00f7238ac3d3093803e66bf2
GLinBoy <hojjatabedie@gmail.com>
2020-10-11 13:27:46 +0330

    init

---
+++ src/main/java/com/glinboy/jpress/JPressAllInOneApplication.java
@@ -0,0 +1,13 @@
+package com.glinboy.jpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPressAllInOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JPressAllInOneApplication.class, args);
+	}
+
+}
