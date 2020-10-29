268eb729fe6116eb2041c620262712d54835fd32
GLinBoy <hojjatabedie@gmail.com>
2020-10-29 18:54:45 +0330

    add posts table to admin panel

--- src/main/java/com/glinboy/jpress/controller/PostController.java
+++ src/main/java/com/glinboy/jpress/controller/PostController.java
@@ -1,7 +1,9 @@
 package com.glinboy.jpress.controller;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,5 +33,25 @@ public class PostController {
 		Post post = postApi.getPostByPostName(postName);
 		model.addAttribute("post", post);
 		return "post.html";
+	}
+	
+	@RequestMapping("admin/posts/new")
+	public String newPosts(Model model) {
+		return "new-post.html";
+	}
+	
+	@RequestMapping("admin/posts/save")
+	public String newPosts(Model model, Post post) {
+		postApi.save(post);
+		Page<Post> page = postApi.getAll(PageRequest.of(0, 20, Sort.by("createdOn").descending()));
+		model.addAttribute("page", page);
+		return "admin-posts.html";
+	}
+	
+	@RequestMapping("admin/posts")
+	public String getAllPostsForAdmin(Model model, Pageable pageable) {
+		Page<Post> page = postApi.getAll(pageable);
+		model.addAttribute("page", page);
+		return "admin-posts.html";
 	}
 }
