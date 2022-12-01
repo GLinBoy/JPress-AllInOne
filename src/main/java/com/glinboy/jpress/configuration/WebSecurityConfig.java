a062b1bf288e158cd0df27f5b1b0efd613ee3805
GLinBoy <hojjatabedie@gmail.com>
2022-12-01 12:21:30 +0100

    👌 IMPROVE: Replaced old configuration code with new config

--- src/main/java/com/glinboy/jpress/configuration/WebSecurityConfig.java
+++ src/main/java/com/glinboy/jpress/configuration/WebSecurityConfig.java
@@ -1,56 +1,71 @@
 package com.glinboy.jpress.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.glinboy.jpress.service.UserServiceApi;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+public class WebSecurityConfig {
 
 	private final UserServiceApi userServiceApi;
 
-	public WebSecurityConfig(BCryptPasswordEncoder bCryptPasswordEncoder, UserServiceApi userServiceApi) {
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	public WebSecurityConfig(UserServiceApi userServiceApi) {
 		this.userServiceApi = userServiceApi;
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userServiceApi).passwordEncoder(bCryptPasswordEncoder);
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		http
+//			.authorizeRequests()
+//			.antMatchers("/admin/**").authenticated()
+//			.antMatchers("/**").permitAll()
+//			.and().csrf().disable().formLogin()
+//			.loginPage("/login").failureUrl("/login?error=true")
+//			.defaultSuccessUrl("/admin")
+//			.usernameParameter("user_name")
+//			.passwordParameter("password")
+//			.and().logout()
+//			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//			.logoutSuccessUrl("/").and().exceptionHandling()
+//			.accessDeniedPage("/access-denied");
 		http
-			.anonymous()
-			.and()
-			.authorizeRequests()
-			.antMatchers("/admin/**").authenticated()
-			.antMatchers("/**").permitAll()
-			.and().csrf().disable().formLogin()
-			.loginPage("/login").failureUrl("/login?error=true")
-			.defaultSuccessUrl("/admin")
-			.usernameParameter("user_name")
-			.passwordParameter("password")
-			.and().logout()
-			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-			.logoutSuccessUrl("/").and().exceptionHandling()
-			.accessDeniedPage("/access-denied");
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/admin/**").authenticated()
+				.requestMatchers("/**").permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin((form) -> form
+				.loginPage("/login")
+				.usernameParameter("user_name")
+				.passwordParameter("password")
+				.successForwardUrl("/admin")
+				.failureUrl("/login?error=true")
+				.permitAll()
+			)
+			.logout((logout) -> logout
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/")
+				.permitAll()
+			);
+		return http.build();
 	}
 
-	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**",
+		web.ignoring().requestMatchers("/resources/**", "/static/**", "/css/**",
 				"/js/**", "/images/**", "/img/**", "/webjars/**", "/h2-console/**");
 	}
 }
