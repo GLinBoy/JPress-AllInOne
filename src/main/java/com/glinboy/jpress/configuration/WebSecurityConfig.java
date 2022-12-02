7b94f30a3d698e32394768f32852081ed67ea6b0
GLinBoy <hojjatabedie@gmail.com>
2022-12-02 12:11:16 +0100

    🐛 FIX: /h2-console page accessible in the security
    resolved #58

--- src/main/java/com/glinboy/jpress/configuration/WebSecurityConfig.java
+++ src/main/java/com/glinboy/jpress/configuration/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,6 +15,22 @@ import com.glinboy.jpress.service.UserServiceApi;
 public class WebSecurityConfig {
 
 	private final UserServiceApi userServiceApi;
+	
+	private static final String[] AUTH_WHITELIST = {
+			"/",
+			"/**"
+	};
+	
+	private static final String[] IGNORE_WHITELIST = {
+			"/resources/**",
+			"/static/**",
+			"/css/**",
+			"/js/**",
+			"/images/**",
+			"/img/**",
+			"/webjars/**",
+			"/h2-console/**"
+	};
 
 	public WebSecurityConfig(UserServiceApi userServiceApi) {
 		this.userServiceApi = userServiceApi;
@@ -29,26 +44,16 @@ public class WebSecurityConfig {
 
 	@Bean
 	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//		http
-//			.authorizeRequests()
-//			.antMatchers("/admin/**").authenticated()
-//			.antMatchers("/**").permitAll()
-//			.and().csrf().disable().formLogin()
-//			.loginPage("/login").failureUrl("/login?error=true")
-//			.defaultSuccessUrl("/admin")
-//			.usernameParameter("user_name")
-//			.passwordParameter("password")
-//			.and().logout()
-//			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//			.logoutSuccessUrl("/").and().exceptionHandling()
-//			.accessDeniedPage("/access-denied");
 		http
-			.authorizeHttpRequests((requests) -> requests
+			.csrf().ignoringRequestMatchers(IGNORE_WHITELIST).disable()
+			.headers().frameOptions().sameOrigin()
+			.and()
+			.authorizeHttpRequests( requests -> requests
 				.requestMatchers("/admin/**").authenticated()
-				.requestMatchers("/**").permitAll()
+				.requestMatchers(AUTH_WHITELIST).permitAll()
 				.anyRequest().authenticated()
 			)
-			.formLogin((form) -> form
+			.formLogin( form -> form
 				.loginPage("/login")
 				.usernameParameter("user_name")
 				.passwordParameter("password")
@@ -56,16 +61,11 @@ public class WebSecurityConfig {
 				.failureUrl("/login?error=true")
 				.permitAll()
 			)
-			.logout((logout) -> logout
+			.logout( logout -> logout
 				.logoutUrl("/logout")
 				.logoutSuccessUrl("/")
 				.permitAll()
 			);
 		return http.build();
-	}
-
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().requestMatchers("/resources/**", "/static/**", "/css/**",
-				"/js/**", "/images/**", "/img/**", "/webjars/**", "/h2-console/**");
 	}
 }
