fc18151de32d5e8d9f758f1ab54fcc747284cbad
GLinBoy <glinboy@live.com>
2026-03-06 18:58:29 +0100

    👌 IMPROVE: Upgrade Spring Boot to 4.0.3
    resolved #127

--- src/main/java/com/glinboy/jpress/model/Meta.java
+++ src/main/java/com/glinboy/jpress/model/Meta.java
@@ -1,14 +1,11 @@
 package com.glinboy.jpress.model;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @MappedSuperclass
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Data
 @EqualsAndHashCode(callSuper = true)
 public abstract class Meta extends Auditable {
